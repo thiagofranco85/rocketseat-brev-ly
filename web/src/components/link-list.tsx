fd2821f42b149deea12455f6d823241fe4c2561b
Thiago Franco <thiago.boo@gmail.com>
2026-08-21 11:06:38 -0300

    fix(web): adjust responsive spacing in link-list and my-links components

--- web/src/components/link-list.tsx
+++ web/src/components/link-list.tsx
@@ -8,7 +8,7 @@ type LinkListProps = {
 
 export function LinkList({ links, onDelete }: LinkListProps) {
   return (
-    <ul className="divide-y divide-gray-200">
+    <ul className="-mb-2.75 divide-y divide-gray-200 lg:-mb-3.75">
       {links.map((link) => (
         <LinkItem key={link.id} link={link} onDelete={onDelete} />
       ))}
