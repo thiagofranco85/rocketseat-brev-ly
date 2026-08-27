6721f996eb4982ff945e1fc051cdad59ac0b4699
Thiago Franco <thiago.boo@gmail.com>
2026-08-27 09:37:00 -0300

    feat(server): add url_shortener schema, migration files and fix docker port mapping

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,1 +1,12 @@
-export {}
+import { sql } from 'drizzle-orm'
+import { integer, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+
+export const urlShortener = pgTable('url_shortener', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`uuidv7()`),
+  originalUrl: text('original_url').notNull(),
+  shortUrl: text('short_url').notNull().unique(),
+  accessCount: integer('access_count').notNull().default(0),
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+})
